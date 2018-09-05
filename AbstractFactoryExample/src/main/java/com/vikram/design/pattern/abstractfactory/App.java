3c79a38fc0137eb4ce488abf27b38a5cf15cbe32
juivikram <juivikram@VikramPC>
2018-09-05 22:43:05 +0530

    Initial project structure

---
+++ AbstractFactoryExample/src/main/java/com/vikram/design/pattern/abstractfactory/App.java
@@ -0,0 +1,13 @@
+package com.vikram.design.pattern.abstractfactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
